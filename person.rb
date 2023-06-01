047a44305f4a96d159d46fcc4f2f16b95753bbec
LoviMadaan <119787828+LoviMadaan@users.noreply.github.com>
2023-06-01 17:30:42 +0530

    Person, Teacher and Student class addition in different files

---
+++ person.rb
@@ -0,0 +1,19 @@
+class Person
+    attr_reader :id, :name, :age
+
+    def initialize(name = "Unknown", age = nil, parent_permission = true)
+        @id = rand(1...100)
+        @name = name 
+        @age = age || 0
+        @parent_permission = parent_permission
+    end 
+
+    def of_age?
+        @age >= 18
+    end 
+
+    def can_use_services?
+        of_age? || parent_permission
+    end
+end 
+
---
+++ student.rb
@@ -0,0 +1,12 @@
+class Student < Person
+    attr_reader :classroom
+
+    def initialize (name, age, parent_permission, classroom)
+        super(name, age, parent_permission)
+        @classroom = classroom
+    end
+
+    def play_hooky
+        "¯\\_(ツ)_/¯"
+    end 
+end 
