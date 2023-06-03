74ce298a51b57ef58b9062db8a0333549a9c5783
LoviMadaan <119787828+LoviMadaan@users.noreply.github.com>
2023-06-03 17:17:57 +0530

    Guard addition to cross check alreday existed classrooms

--- student.rb
+++ student.rb
@@ -13,6 +13,6 @@ class Student < Person
 
   def classroom=(classroom)
     @classroom = classroom
-    classroom.students << self
+    classroom.students.push(self) unless classroom.students.include?(self)
   end
 end
