b28409d4ccf3ff0e1bda4874e8aa3b88d22cb0b3
LoviMadaan <119787828+LoviMadaan@users.noreply.github.com>
2023-06-03 19:58:03 +0530

    Rental file code updated

--- rental.rb
+++ rental.rb
@@ -1,10 +1,13 @@
 class Rental
-  attr_accessor :date, :person, :book
+  attr_reader :person, :book
+  attr_accessor :date
 
   def initialize(date, person, book)
     @date = date
+
     @person = person
     person.rentals << self
+
     @book = book
     book.rentals << self
   end
