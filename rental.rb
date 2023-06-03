e7ead66a55130a97b9641009d4b9722cdef36f93
LoviMadaan <119787828+LoviMadaan@users.noreply.github.com>
2023-06-03 19:11:59 +0530

    rental file code updated

--- rental.rb
+++ rental.rb
@@ -1,14 +1,11 @@
-require_relative 'person'
-
-class Rental < Person
-  attr_accessor :date, :book, :person
-
-  def initialize(date, book, person)
-    @date = date
-    @book = book
-    @person = person
-
-    book.rentals << self
-    person.rentals << self
-  end
+class Rental
+    attr_accessor :date, :person, :book
+  
+    def initialize(date, person, book)
+      @date = date
+      @person = person
+      person.rentals << self
+      @book = book
+      book.rentals << self
+    end
 end
