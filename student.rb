1442d062abae27f798e620ab7300d0d18284e5c4
LoviMadaan <119787828+LoviMadaan@users.noreply.github.com>
2023-06-06 17:16:39 +0530

    Update student.rb

--- student.rb
+++ student.rb
@@ -10,14 +10,10 @@ class Student < Person
 
   def classroom=(classroom)
     @classroom = classroom
-<<<<<<< refractor
     classroom.add_student(self) unless classroom.students.include?(self)
   end
 
   def play_hooky
     '¯(ツ)/¯'
-=======
-    classroom.students.push(self) unless classroom.students.include?(self)
->>>>>>> dev
   end
 end
