de9973a2d44441a1659daa7da018b559f0a49456
LoviMadaan <119787828+LoviMadaan@users.noreply.github.com>
2023-06-06 14:12:02 +0530

    Conflict fixed

--- students.rb
+++
@@ -1,8 +0,0 @@
-require_relative 'person'
-
-class Student < Person
-  def initialize(name, age, classroom = nil, parent_permission: true)
-    super(name, age, parent_permission: parent_permission)
-    @classroom = classroom
-  end
-end
