892f07ff47ebafd2cb503da9e64be1ada8dcf29b
krisbobo <kiyongbohbo@gmail.com>
2023-06-08 21:19:44 +0100

    Add tests for the teacher and trimmer classes

---
+++ spec/trimmer_decorator_spec.rb
@@ -0,0 +1,14 @@
+require_relative '../trimmer_decorator'
+require_relative '../person'
+require_relative '../base_decorator'
+require 'rspec'
+
+describe TrimmerDecorator do
+  context 'When the name is a string characters' do
+    it 'it takes a string and returns an instance of it' do
+    person = Person.new(20, 'Aaradhya', 'Madaan')
+    trimmer = TrimmerDecorator.new(person)
+    expect(trimmer).to be_instance_of TrimmerDecorator
+    end
+  end
+end
